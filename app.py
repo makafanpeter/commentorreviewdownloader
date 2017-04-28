965b95bf77d5692c7be1af2cf6948b0bc268aa82
VENTUREGARDEN\peter.makafan <makafanpeter@gmail.com>
2017-04-28 05:08:56 +0100

    Worked on Model, and Db persisitence

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-#from  reviewparser import ReviewParser
-import  re
+import csv
+import re
+import io
+
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
-from flask import Flask, render_template, request, jsonify, url_for, redirect, abort
+from flask import Flask, render_template, request, jsonify, url_for, redirect, abort, make_response
 from rq import Queue
 from rq.job import Job
 
 import configuration
+from reviewparser import ReviewParser
 from worker import conn
-
-
 
 app = Flask(__name__)
 app.config.from_object(configuration.ProductionConfig)
 db = SQLAlchemy(app)
 
-
 task_queue = Queue(connection=conn)
 
 from models import *
+
 
 @app.route('/')
 def index():
@@ -44,7 +45,7 @@ def get_reviews():
         parser_name = 'amazon'
     else:
         abort(400)
-    parser = ReviewParser.get_parser(parser_name)
+    parser =  ReviewParser.get_parser(parser_name)
 
     # start job
     job = task_queue.enqueue_call(
@@ -53,6 +54,7 @@ def get_reviews():
     # return created job id
     return job.get_id()
 
+
 @app.route("/result/<job_key>", methods=['GET'])
 def get_result(job_key):
     job = Job.fetch(job_key, connection=conn)
@@ -60,10 +62,28 @@ def get_result(job_key):
     if job.is_finished:
         if type(job.result) == dict:
             if "error" in job.result:
-              return jsonify(job.result), 500
+                return jsonify(job.result), 500
         return jsonify({"id": job.result})
     else:
         return "Nay!", 202
+
+
+@app.route('/download/<int:id>',methods=['get'])
+def download_csv(id):
+    dest = io.StringIO()
+    #writer = csv.writer(dest)
+    reviews = Review.query.filter_by(item_id=id).all()
+    header = ["user_name", "date", "star_rating", "review", "url"]
+    writer = csv.DictWriter(dest, delimiter=",", fieldnames=header)
+    writer.writeheader()
+    for row in reviews:
+        writer.writerow(row.serialize)
+
+    output = make_response(dest.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
+
 
 if __name__ == '__main__':
     app.run(threaded=True,
--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gunicorn app:app --daemon
+python worker.py
 
